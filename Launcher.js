4146f4f8cc1ad7e7b338130884f746bc8eb57241
Tanv243 <77434020+Tanv243@users.noreply.github.com>
2021-04-30 13:10:39 +0530

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,32 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB:pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.Launcher = Constraint.create(options);
+        World.add(world, this.Launcher);
+        this.pointB=pointB;
+    }
+    fly(){
+        this.Launcher.bodyA=null;
+
+    }
+    attach(){
+        this.Launcher.bodyA=Body;
+    }
+     
+    display(){
+        if(this.Launcher.bodyA){
+            var pointA = this.Launcher.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+    
+        }
+       
+    }
+    
+}
